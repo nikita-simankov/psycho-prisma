322b8fc6fc5649111b144aa378b3be6a1f543b8b
Никита Симаньков <simankov.contact@gmail.com>
2024-11-04 17:47:02 +0300

    initial commit

--- src/app/dashboard/components/user-button.tsx
+++ src/app/dashboard/components/user-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,8 +10,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { User } from "@prisma/client";
-import { useMutation } from "@tanstack/react-query";
 import { LogOut, UserCircle, Users2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 type Properties = {
@@ -19,19 +19,23 @@ type Properties = {
 };
 
 export const DashboardNavbarUserButton: React.FC<Properties> = ({ user }) => {
+  const router = useRouter();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Avatar>
-          <AvatarFallback>
-            {user.lastName.at(0)! + user.name.at(0)!}
-          </AvatarFallback>
+          <AvatarImage src={user.imageURL} />
+          <AvatarFallback>{user.name[0] + user.surname[0]}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent>
         <DropdownMenuGroup>
-          <DropdownMenuItem className="flex flex-row items-center gap-2">
+          <DropdownMenuItem
+            className="flex flex-row items-center gap-2"
+            onClick={() => router.push("/dashboard/users/" + user.id)}
+          >
             <UserCircle className="w-[1.2rem] h-[1.2rem]" />
             Мой профиль
           </DropdownMenuItem>
@@ -40,11 +44,10 @@ export const DashboardNavbarUserButton: React.FC<Properties> = ({ user }) => {
         <DropdownMenuSeparator />
 
         <DropdownMenuGroup>
-          <DropdownMenuItem className="flex flex-row items-center gap-2">
-            <Users2 className="w-[1.2rem] h-[1.2rem]" />
-            Сменить аккаунт
-          </DropdownMenuItem>
-          <DropdownMenuItem className="flex flex-row items-center gap-2 text-red-500">
+          <DropdownMenuItem
+            className="flex flex-row items-center gap-2 text-red-500"
+            asChild
+          >
             <LogOut className="w-[1.2rem] h-[1.2rem] text-red-500" />
             Выйти
           </DropdownMenuItem>
